59280ee69bd59bd056d295aaf7667376f75fd07c
mascaroa <mascaroa@physics.mcgill.ca>
2018-08-22 16:53:21 -0400

    Updated run script for folders...

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-cd /ltmp/mascaroa/decay_time_sims/
 cat out.txt > log.txt
 rm out.txt
-/ltmp/matlab/8.3/bin/matlab -nodisplay -nosplash < /decay_time_sims/inputs.m >> out.txt &
+
+# MATLAB path goes here:
+
+/ltmp/matlab/8.3/bin/matlab -nodisplay -nosplash < decay_time_sims/inputs.m >> out.txt &
 tail -f out.txt
