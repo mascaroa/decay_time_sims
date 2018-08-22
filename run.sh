d191a979090a495f76115057a2bebef36a9d17c8
mascaroa <mascaroa@physics.mcgill.ca>
2018-08-22 15:59:40 -0400

    Run script updated

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 cd /ltmp/mascaroa/decay_time_sims/
-matlab -nodisplay -nosplash < inputs.m >> out.txt
+cat out.txt > log.txt
+rm out.txt
+/ltmp/matlab/8.3/bin/matlab -nodisplay -nosplash < inputs.m >> out.txt &
+tail -f out.txt
