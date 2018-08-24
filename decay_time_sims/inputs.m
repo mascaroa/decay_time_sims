6edf26870076896bd233223871642b01077d3e5a
mascaroa <mascaroa@physics.mcgill.ca>
2018-08-24 16:38:45 -0400

    Q=5000, wider td range

--- decay_time_sims/inputs.m
+++ decay_time_sims/inputs.m
@@ -5,7 +5,7 @@ addpath(genpath('/ltmp/mascaroa/decay_time_sims'))
 % TODO I guess
 
 tp = [100e-9,500e-9,1000e-9,1500e-9,3000e-9];
-td = [1:6]*1e-8;
+td = [0:6:36]*1e-7;
 tau = 300e-9;
 fprintf('\n\nStarting script....\n\n')
 run_sim_pulse(tp,td,tau)
